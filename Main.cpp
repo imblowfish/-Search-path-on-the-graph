3b1e684a0f60a6f3cce88aac041a53c7b7f9d052
imblowfish <stagg7777@gmail.com>
2019-03-27 02:27:46 +0300

    Rename Lab2.cpp to Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+﻿#include "stdafx.h"
+#include "Main.h"
+#include "MainMenu.h"
+using namespace std;
+
+int main(int argc, char *argvp[]){
+	if(!initSDL()){
+		system("pause");
+		return -1;
+	} else cout << "initSDL() success" << endl;
+	MainMenu menu("mainMenu", true);
+	//menu.SetPathToDir(argvp[0]);
+	menu.LinkRenderer(renderer);
+	menu.InitMenu();
+	menu.MainLoop();
+	outSDL();
+	return 0;
+}
+
